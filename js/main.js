aeaea243014ab33454cdad0c60dab66cdc2a1f2e
ekdora <dora.egrykiss@gmail.com>
2022-08-15 16:29:49 +0200

    Second and third tasks solutions

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@
 - A függvény neve `generateList` legyen!
 - A HTML-template egy lista, amelynek a listaelemei a kapott tömb értékeit tartalmazzák.
 */
+
+const listArray = ["első", "második", "harmadik"];
+
+const generateList = (array) => {
+	listElement = document.createElement('ul');
+	document.getElementsByTagName('body')[0].appendChild(listElement);
+	for (i = 0; i < array.length; ++i) {
+			listItem = document.createElement('li');
+			listItem.innerHTML = array[i];
+			listElement.appendChild(listItem);
+	}
+}
+
+generateList(listArray);
+
+export default generateList;
